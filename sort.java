d205e9b26ecdae295a8be48cd6e02869e6a0adf7
Clown Monster <53732385+ClownMonster@users.noreply.github.com>
2019-10-17 07:39:46 +0530

    updating the code

--- sort.java
+++ sort.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class sort{
 
-  void sort(int A[])
+  sort(int A[])   /*constructor */
   {
-    int n = A.length;
+    int n = A.length;   /*sorting algorithm for asscending order*/
     for(int i=0;i<n-1;i++)
       for(int j=0;j<n-i-1;j++)
-       if(A[j] > A[j+1])
+       if(A[j] > A[j+1])    
         {
           int temp = A[j];
           A[j] = A[j+1];
-          A[j+1] = temp;
+          A[j+1] = temp;    /* interchange of elements */
         }        
   }
 
@@ -25,23 +25,26 @@ class sort{
   public static void main(String args[])
   { 
     Scanner sc = new Scanner(System.in);
+    
     System.out.println("Enter the size of the array");
     int size = sc.nextInt();
+    
     System.out.println("Enter the elments of the array");
     int A[] = new int[size];
-    for(int i=0;i<size;i++){
+    
+    for(int i=0;i<size;i++){  /*taking Array inputs*/
      A[i] = sc.nextInt();}
-    sort o = new sort();
-    o.sort(A);
+    
+    sort o = new sort(A); /* object refernce to sort class and Sorting the array using constructor */
+    
     System.out.println("Sorted array :");
+    
     o.print(A);
+    
     sc.close();
 
 
 
   }
-
-
-
 
 }

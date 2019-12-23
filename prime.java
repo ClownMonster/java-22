c1ef03cb2db3875dc7313c3827d9a86dd090d51b
Clown Monster <mk6386223@gmail.com>
2019-12-23 19:52:12 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -7,13 +7,13 @@ public class prime {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the number to be checked");
       int a = sc.nextInt();
-      boolean flag = true;
+      boolean flag = true; /* initializing flag to true value */
       if(a==1 || a == 0)
       System.out.println(a+ " is not a prime number");
       else{
       for(int i=2; i<=a/2; i++)
          {
-           if(a%i==0) flag = false;
+           if(a%i==0) flag = false; /* changing flagg to flase value if not prime */
          }
 
          if(flag)
